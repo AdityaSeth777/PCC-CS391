228a6230fc56df0b7eb5854679478bd5c23d1129
Aditya Seth <adityaseth.cse@gmail.com>
2023-03-03 10:33:42 +0530

    8 added

---
+++ 8 - PQ - Enq and Deq/PQ_Enq&Deq.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+// Definition of the Priority Queue node
+struct Node {
+    int data;
+    int priority;
+};
+
+// Create a new node with the given data and priority
+struct Node* newNode(int data, int priority) {
+    struct Node* node = (struct Node*) malloc(sizeof(struct Node));
+    node->data = data;
+    node->priority = priority;
+    return(node);
+}
+
+// Check if the given priority queue is empty
+int isEmpty(struct Node* queue[], int front, int rear) {
+    if (front > rear) {
+        return 1;
+    }
+    return 0;
+}
+
+// Check if the given priority queue is full
+int isFull(int rear) {
+    if (rear == MAX_SIZE - 1) {
+        return 1;
+    }
+    return 0;
+}
+
+// Enqueue a new node with the given data and priority
+void enqueue(struct Node* queue[], int *front, int *rear, int data, int priority) {
+    struct Node* node = newNode(data, priority);
+    if (isFull(*rear)) {
+        printf("Queue is full. Cannot enqueue any more elements.\n");
+        return;
+    }
+    if (*front == -1 && *rear == -1) {
+        *front = *rear = 0;
+        queue[*rear] = node;
+    } else {
+        int i;
+        for (i = *rear; i >= *front; i--) {
+            if (node->priority > queue[i]->priority) {
+                queue[i+1] = queue[i];
+            } else {
+                break;
+            }
+        }
+        queue[i+1] = node;
+        (*rear)++;
+    }
+    printf("%d with priority %d has been enqueued.\n", data, priority);
+}
+
+// Dequeue the node with the highest priority from the given priority queue
+void dequeue(struct Node* queue[], int *front, int *rear) {
+    if (isEmpty(queue, *front, *rear)) {
+        printf("Queue is empty. Cannot dequeue any element.\n");
+        return;
+    }
+    struct Node* node = queue[*front];
+    printf("%d with priority %d has been dequeued.\n", node->data, node->priority);
+    if (*front == *rear) {
+        *front = *rear = -1;
+    } else {
+        (*front)++;
+    }
+}
+
+int main() {
+    int front = -1, rear = -1;
+    struct Node* queue[MAX_SIZE];
+
+    while (1) {
+        printf("1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        int choice;
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1: {
+                int data, priority;
+                printf("Enter the data to enqueue: ");
+                scanf("%d", &data);
+                printf("Enter the priority: ");
+                scanf("%d", &priority);
+                enqueue(queue, &front, &rear, data, priority);
+                break;
+            }
+            case 2: {
+                dequeue(queue, &front, &rear);
+                break;
+            }
+            case 3: {
+                exit(0);
+            }
+            default: {
+                printf("Invalid choice. Try again.\n");
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ 8 - PQ - Enq and Deq/Readme.md
@@ -0,0 +1,11 @@
+The code implements a Priority Queue data structure using an array of structures. Each structure contains two fields - data and priority. The priority queue is implemented in such a way that the elements are arranged in descending order of priority. This means that the element with the highest priority will be dequeued first.
+
+The code defines a struct Node that represents a single node in the priority queue. The struct contains two fields - data and priority. The function newNode is used to create a new node with the given data and priority.
+
+The functions isEmpty and isFull are used to check if the priority queue is empty or full, respectively.
+
+The function enqueue is used to add a new node with the given data and priority to the priority queue. If the queue is already full, the function prints an error message and returns. If the queue is empty, the new node is simply added to the front of the queue. If the queue is not empty, the function finds the correct position to insert the new node based on its priority. It does this by comparing the priority of the new node with the priorities of the existing nodes in the queue. The new node is inserted in such a way that the queue remains in descending order of priority.
+
+The function dequeue is used to remove the node with the highest priority from the priority queue. If the queue is empty, the function prints an error message and returns. Otherwise, it removes the node from the front of the queue and returns its data and priority values.
+
+The main function is used to interact with the user and test the enqueue and dequeue functions. It repeatedly prompts the user to choose between enqueue, dequeue, or exit. When the user chooses enqueue, the function asks for the data and priority values of the new node and adds it to the priority queue. When the user chooses dequeue, the function removes the node with the highest priority from the queue and displays its data and priority values. The program exits when the user chooses to exit.
