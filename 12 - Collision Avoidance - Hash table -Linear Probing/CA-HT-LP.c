fcb2ca31d0c2724c9446580c85350e3812ca78c6
Aditya Seth <adityaseth.cse@gmail.com>
2023-03-03 10:54:05 +0530

    12 added

---
+++ 12 - Collision Avoidance - Hash table -Linear Probing/CA-HT-LP.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+int hash(int key) {
+    return key % SIZE;
+}
+
+int linear_probe(int hash_table[], int key) {
+    int index = hash(key);
+
+    while (hash_table[index] != -1 && hash_table[index] != key) {
+        index = (index + 1) % SIZE;
+    }
+
+    return index;
+}
+
+void insert(int hash_table[], int key) {
+    int index = hash(key);
+
+    if (hash_table[index] == -1) {
+        hash_table[index] = key;
+    } else {
+        index = linear_probe(hash_table, key);
+        hash_table[index] = key;
+    }
+}
+
+int search(int hash_table[], int key) {
+    int index = hash(key);
+
+    while (hash_table[index] != -1) {
+        if (hash_table[index] == key) {
+            return index;
+        }
+        index = (index + 1) % SIZE;
+    }
+
+    return -1;
+}
+
+void display(int hash_table[]) {
+    printf("Hash Table:\n");
+    printf("Index\tValue\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d\t%d\n", i, hash_table[i]);
+    }
+}
+
+int main() {
+    int hash_table[SIZE];
+
+    // Initialize hash table with -1
+    for (int i = 0; i < SIZE; i++) {
+        hash_table[i] = -1;
+    }
+
+    int choice, key, index;
+
+    while (1) {
+        printf("\nChoose an operation:\n");
+        printf("1. Insert\n");
+        printf("2. Search\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the key to insert: ");
+                scanf("%d", &key);
+                insert(hash_table, key);
+                break;
+            case 2:
+                printf("Enter the key to search: ");
+                scanf("%d", &key);
+                index = search(hash_table, key);
+                if (index == -1) {
+                    printf("Key not found.\n");
+                } else {
+                    printf("Key found at index %d.\n", index);
+                }
+                break;
+            case 3:
+                display(hash_table);
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
+
+    return 0;
+}
---
+++ 12 - Collision Avoidance - Hash table -Linear Probing/Readme.md
@@ -0,0 +1,15 @@
+```This program demonstrates how to implement collision avoidance in a hash table using linear probing.```
+
+The program defines three main functions:
+
+hash(int key): This function takes a key value as input and returns the hash value by using the modulo operator to ensure that the key is within the range of the hash table.
+
+linear_probe(int hash_table[], int key): This function takes the hash table and the key as inputs and performs linear probing to find the next available index for the key. It starts by computing the initial index using the hash function. If the index is already occupied, the function looks for the next available index by adding 1 to the index and taking the modulus of the hash table size. This process continues until an empty index is found or the key is already in the hash table.
+
+insert(int hash_table[], int key): This function takes the hash table and the key as inputs and inserts the key into the hash table. It uses the hash function to compute the initial index for the key. If the index is empty, the key is inserted at that index. If the index is occupied, the function uses the linear_probe function to find the next available index for the key and inserts the key at that index.
+
+The program also defines a search function that takes the hash table and the key as inputs and searches for the key in the hash table using linear probing. It starts by computing the initial index using the hash function. If the index is occupied and does not contain the key, the function looks for the next index using linear probing until the key is found or an empty index is encountered.
+
+The display function is also defined to display the contents of the hash table.
+
+The main function is responsible for taking user inputs and calling the appropriate functions based on the user's choice. It displays a menu of available operations and prompts the user to choose one. The program exits when the user chooses the "Exit" option.
